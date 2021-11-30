fd4784d926c36094d800aefc86b6d576e52b7445
Ravi Hamidov <ravihamidov42@gmail.com>
2021-11-30 22:39:03 +0400

    Flask-Day-01 Task Completed

--- Works/FlaskTasks/Flask-Day-01/Task-01.py
+++ Works/FlaskTasks/Flask-Day-01/Task-01.py
@@ -1,6 +1,6 @@
 # HomeWork :
     # Web Server, WSGI və HTTP haqqında ilkin məlumatlar araşdırmaq faydalı olabilər
     # HTTP GET request , HTTP POST request haqqında araşdlrma faydalı ola bilər
-    # Framework vs library arasındakı fərqlər
+    # Framework vs Library arasındakı fərqlər
 
-# Movzular arasdirildi ve ReSearch.md file-a yerlesdirildi
+# Movzular arasdirildi ve ReSearch.md file-a yerlesdirildi .
