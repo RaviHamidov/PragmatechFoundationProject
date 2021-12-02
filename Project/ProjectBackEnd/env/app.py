67c288254c4d8c19bd7925378df81cca6bd19812
Ravi Hamidov <ravihamidov42@gmail.com>
2021-12-02 14:28:04 +0400

    Progress

---
+++ Project/ProjectBackEnd/env/app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+from data import cards
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return render_template('index.html',cards=cards)
+
+
+@app.route('/about')
+def about():
+
+    return render_template('about.html')
+
+@app.route('/programs')
+def programs():
+
+    return render_template('programs.html')
+if __name__=='__main__':
+    app.run(debug=True)
