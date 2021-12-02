7371b5caa185128993641ae3af26a5be9d0c2197
Ravi Hamidov <ravihamidov42@gmail.com>
2021-12-02 19:44:41 +0400

    GitHub Organize

--- Project/ProjectBackEnd/app.py
+++ Project/ProjectBackEnd/app.py
@@ -1,22 +1,23 @@
 from flask import Flask,render_template
-from data import cards
+
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
+    count = 19
+    return render_template('index.html',count=count)
 
-    return render_template('index.html',cards=cards)
 
+# @app.route('/about')
+# def about():
 
-@app.route('/about')
-def about():
+#     return render_template('about.html')
 
-    return render_template('about.html')
+# @app.route('/programs')
+# def programs():
 
-@app.route('/programs')
-def programs():
+#     return render_template('programs.html')
 
-    return render_template('programs.html')
 if __name__=='__main__':
     app.run(debug=True)
--- Project/ProjectBackEnd/data.py
+++ Project/ProjectBackEnd/data.py
@@ -1,37 +1,9 @@
-cards=[
-    
-    {
-        'cardHeader':'First Card Header',
-        'cardDetail':'First Card Detail',
-        'cardHeaderName':'headingOne',
-        'cardBodyName':'collapseOne'
-    },
-    {
-        'cardHeader':'Second Card Header',
-        'cardDetail':'Second Card Detail',
-        'cardHeaderName':'headingTwo',
-        'cardBodyName':'collapseTwo'
-    },
-    {
-        'cardHeader':'Third Card Header',
-        'cardDetail':'Third Card Detail',
-        'cardHeaderName':'headingThree',
-        'cardBodyName':'collapseThree'
-    }
-    ,
-    {
-        'cardHeader':'Fourth Card Header',
-        'cardDetail':'Fourth Card Detail',
-        'cardHeaderName':'headingFourth',
-        'cardBodyName':'collapseFourth'
-    }
-]
+from typing import count
 
-services=[
+count=[
     
     {
-        'id':1,
-        'serviceName':'Processing',
-        'serviceImg':'static/images/1.jpg'
+        '':'First Card Header',
+        'cardDetail':'First Card Detail'
     }
 ]
